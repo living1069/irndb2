f2ebeedf4d43254a57e3c158b37f98005a4cb022
Sebastian Schmeier <s.schmeier@gmail.com>
2016-03-31 11:52:39 +1300

    Changed base menu to reflect the RNAs and targets.

--- urls.py
+++ urls.py
@@ -14,6 +14,7 @@ urlpatterns = patterns('',
     url(r'tables/$', views.tables_method, name='tables_url_name'),
     url(r'charts/$', views.charts_method, name='charts_url_name'),                   
     url(r'search/$', views.search_method, name='search_url_name'),
+    url(r'browse/$', views.browse_method, name='browse_url_name'),
     # redirect non-matching urls to home/
     url(r'^.*$', RedirectView.as_view(url='home/', permanent=False), name='index'), 
 )
--- views.py
+++ views.py
@@ -2,27 +2,6 @@ from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
 
 # Create your views here.
-def home_method(request):
-    context = {}
-    return render(request, "irndb/home.html", context)
-
-def contact_method(request):
-    context = {}
-    #return HttpResponse(request.path)
-    return render(request, "irndb/contact.html", context)
-
-def doc_method(request):
-    context = {}
-    return render(request, "irndb/doc.html", context)
-
-def tables_method(request):
-    context = {}
-    return render(request, "irndb/tables.html", context)
-
-def charts_method(request):
-    context = {}
-    return render(request, "irndb/charts.html", context)
-
 def search_method(request):
     context = {}
     if request.method == 'GET':
@@ -39,4 +18,54 @@ def search_method(request):
     # if not GET method return homepage
     else:
         return render(request, "irndb/home.html", context)
+
+def home_method(request):
+    context = {}
+    return render(request, "irndb/home.html", context)
+
+def contact_method(request):
+    context = {}
+    #return HttpResponse(request.path)
+    return render(request, "irndb/contact.html", context)
+
+def doc_method(request):
+    context = {}
+    sTab = request.GET.get('tab', 'x')
+    context = {}
+    if sTab not in ['1','2','3','4']:
+        return render(request, "irndb/doc.html", context)
+    elif sTab == '1':
+        return render(request, "irndb/doc_overview.html", context)
+    elif sTab == '2':
+        return render(request, "irndb/doc_desc.html", context)
+    elif sTab == '3':
+        return render(request, "irndb/doc_resources.html", context)
+    elif sTab == '4':
+        return render(request, "irndb/doc_stats.html", context)    
+    else:
+        return render(request, "irndb/doc.html", context)
+
+def browse_method(request):
+    context = {}
+    sType = request.GET.get('type', 'x')
+    context = {}
+    if sType == 'mirna':
+        return render(request, "irndb/browse_mirna.html", context)
+    elif sType == 'target':
+        return render(request, "irndb/browse_target.html", context)
+    elif sType == 'lncrna':
+        return render(request, "irndb/browse_lncrna.html", context)
+    elif sType == 'pirna':
+        return render(request, "irndb/browse_pirna.html", context)
+    else:
+        return render(request, "irndb/home.html", context)
+    
+def tables_method(request):
+    context = {}
+    return render(request, "irndb/tables.html", context)
+
+def charts_method(request):
+    context = {}
+    return render(request, "irndb/charts.html", context)
+
 
