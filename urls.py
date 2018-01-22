cf29dbffbcd85ed3af1ef098882eada76098ab39
Sebastian Schmeier <s.schmeier@gmail.com>
2018-01-22 22:31:36 +0000

    remove pattern lin

--- urls.py
+++ urls.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from django.conf.urls.static import static
 from django.views.decorators.cache import cache_page
 from django.views.generic.base import RedirectView
